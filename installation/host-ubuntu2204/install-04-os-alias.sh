8bf40259555733f34e986a27daaf07b3ef504225
CS3-Zhang <48177679+Joy-Zhang-0303@users.noreply.github.com>
2026-07-16 12:45:09 +0900

    Update and rename install-05-os-alias.sh to install-04-os-alias.sh
    fix alias error

--- installation/host-ubuntu2204/install-04-os-alias.sh
+++ installation/host-ubuntu2204/install-04-os-alias.sh
@@ -1,10 +1,10 @@
 export COPPELIASIM_ROOT_DIR=/home/atom/Documents/CoppeliaSim_Edu_V4_10_0_rev0_Ubuntu22_04
 echo 'export COPPELIASIM_ROOT_DIR=/home/atom/Documents/CoppeliaSim_Edu_V4_10_0_rev0_Ubuntu22_04' >> ~/.bashrc
-echo 'alias sim='"${COPPELIASIM_ROOT_DIR}/coppeliaSim.sh" -f /home/atom/Documents/Reaction-Wheel-Pendulum-Controller-Simulations/simulations/coppeliasim_simulation/scene.ttt'' >> ~/.bashrc
+echo "alias sim='"${COPPELIASIM_ROOT_DIR}/coppeliaSim.sh" -f /home/atom/Documents/Reaction-Wheel-Pendulum-Controller-Simulations/simulations/coppeliasim_simulation/scene.ttt'" >> ~/.bashrc
 
 # DL simulator and install
 cd ~/Documents/
-wget tbc
+# wget tbc
 tar -xf CoppeliaSim_Edu_V4_10_0_rev0_Ubuntu22_04.tar.xz
 cd CoppeliaSim_Edu_V4_10_0_rev0_Ubuntu22_04
 ./coppeliaSim.sh
